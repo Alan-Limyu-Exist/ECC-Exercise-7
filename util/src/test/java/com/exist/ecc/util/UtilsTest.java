4c893e4c02e57451b597ba0b586e1f058eae9495
Alan-Limyu-Exist <alan.limyu@exist.com>
2024-09-20 10:18:48 +0800

    Refactor UtilsTest remove redundant code

--- util/src/test/java/com/exist/ecc/util/UtilsTest.java
+++ util/src/test/java/com/exist/ecc/util/UtilsTest.java
@@ -1,27 +1,19 @@
 import com.exist.ecc.util.Utils;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.io.InputStream;
 import java.io.PrintStream;
 
 import java.util.Scanner;
 
-import java.lang.reflect.Field;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 
 public class UtilsTest {
-    private final PrintStream originalOut = System.out;
-    private final InputStream originalIn = System.in;
     private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
 
     @BeforeEach
@@ -36,7 +28,7 @@ public class UtilsTest {
         public void testValidInput(String input) throws java.io.IOException{
             String invalidMessage = "Error message for invalid input";
 
-            resetScanner(new ByteArrayInputStream(input.getBytes()));
+            Utils.SCANNER = new Scanner(new ByteArrayInputStream(input.getBytes()));
 
             assertEquals(Integer.parseInt(input), Utils.getUnsignedIntInput(invalidMessage), "The input should be valid");
             assertEquals("", outputStreamCaptor.toString().trim(), "No error message should be printed");
@@ -47,7 +39,7 @@ public class UtilsTest {
         public void testInvalidInput(String input) {
             String invalidMessage = "Error message for invalid input";
 
-            resetScanner(new ByteArrayInputStream(input.getBytes()));
+            Utils.SCANNER = new Scanner(new ByteArrayInputStream(input.getBytes()));
 
             assertEquals(-1, Utils.getUnsignedIntInput(invalidMessage), "The input should be invalid");
             assertEquals(invalidMessage, outputStreamCaptor.toString().trim(), "Error message should be printed");
@@ -59,23 +51,12 @@ public class UtilsTest {
     public void testGetStringInput(String input) {
         String message = "Enter a string: ";
 
-        resetScanner(new ByteArrayInputStream(input.getBytes()));
+        Utils.SCANNER = new Scanner(new ByteArrayInputStream(input.getBytes()));
 
         String returnedInput = Utils.getStringInput(message);
         String outputtedMessage = outputStreamCaptor.toString();
 
         assertEquals(message, outputtedMessage, "The message prompt should be printed");
         assertEquals(message + input, outputtedMessage + returnedInput, "The input should match the expected string");
-    }
-
-    @AfterEach
-    public void tearDown() {
-        System.setOut(originalOut);
-        System.setIn(originalIn);
-    }
-
-    private void resetScanner(ByteArrayInputStream inputStream) {
-        outputStreamCaptor.reset();
-        Utils.SCANNER = new Scanner(inputStream);
     }
 }
